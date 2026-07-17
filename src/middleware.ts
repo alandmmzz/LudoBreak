de4de9dcb7cb5d88f0c3ec874f2e1da846a054c6
Aland Martínez <alandmarp11@gmail.com>
2026-07-17 16:19:39 -0300

    Borrar código viejo (auth, grupos, stats, BGG)

--- src/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { type NextRequest, NextResponse } from 'next/server'
-
-export const config = {
-  matcher: [],
-}
-
-export async function middleware(request: NextRequest) {
-  return NextResponse.next()
-}
